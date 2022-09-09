e2a918e7371e1ba59abd10bbdd5b2ee653b0733e
RUTH <ruthmuasya2000@gmail.com>
2022-09-09 17:54:11 +0300

    Events-Application

--- app/src/main/java/com/example/realtimeschedule/MainActivity.java
+++ app/src/main/java/com/example/realtimeschedule/MainActivity.java
@@ -4,14 +4,27 @@ package com.example.realtimeschedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+    Button services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        services= findViewById(R.id.btnServices);
+        services.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent admin= new Intent(MainActivity.this, AdminActivity.class);
+                startActivity(admin);
+            }
+        });
     }
 }
